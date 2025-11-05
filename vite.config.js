893fdf8ecbbb7c0b6bbab451114f9eafd12cb24a
lilyrose1013 <148171335+lilyrose1013@users.noreply.github.com>
2025-11-04 22:08:44 -0500

    Fix base path in vite.config.js to match repository name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Assignment3_LilyVosari_Lab/'
+  base: '/Assiganmen3_Lab_LilyVosari/'
 })
